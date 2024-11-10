0c70ac0879d394cb9b1eeeabe59ab503530b3a3c
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-11 06:57:10 +0900

    project init

---
+++ ScheduleManage/src/main/java/com/example/schedule/ScheduleManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManageApplication.class, args);
+    }
+
+}
