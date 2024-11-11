188cfc70ab9740e2ed51a5e7f68dc297bdbf0111
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-11 14:11:20 +0900

    Feat : Define findScheduleByAuthorAndModificationDate func to ScheduleRepository

--- ScheduleManage/src/main/java/com/example/schedule/repository/ScheduleRepository.java
+++ ScheduleManage/src/main/java/com/example/schedule/repository/ScheduleRepository.java
@@ -1,6 +1,12 @@
 package com.example.schedule.repository;
 
+import com.example.schedule.dto.ScheduleResponseDto;
+
+import java.util.List;
+
 public interface ScheduleRepository {
 
     long saveSchedule(Long id, String todo);
+
+    List<ScheduleResponseDto> findScheduleByAuthorAndModificationDate(String author, String modificationDate);
 }
