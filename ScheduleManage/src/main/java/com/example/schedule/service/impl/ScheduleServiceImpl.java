9749e56c7c5693c3310c532c113b3ee9c751c870
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-11 14:11:00 +0900

    Feat : Implement findAll func to ScheduleServiceImpl

--- ScheduleManage/src/main/java/com/example/schedule/service/impl/ScheduleServiceImpl.java
+++ ScheduleManage/src/main/java/com/example/schedule/service/impl/ScheduleServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.schedule.service.impl;
 
 import com.example.schedule.dto.ScheduleRequestDto;
+import com.example.schedule.dto.ScheduleResponseDto;
 import com.example.schedule.entity.User;
 import com.example.schedule.repository.ScheduleRepository;
 import com.example.schedule.repository.UserRepository;
 import com.example.schedule.service.ScheduleService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
@@ -30,5 +32,10 @@ public class ScheduleServiceImpl implements ScheduleService {
         user.orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저입니다"));
 
         return scheduleRepository.saveSchedule(user.get().getId(), scheduleRequestDto.getTodo());
+    }
+
+    @Override
+    public List<ScheduleResponseDto> findAll(String author, String modificationDate) {
+        return scheduleRepository.findScheduleByAuthorAndModificationDate(author, modificationDate);
     }
 }
