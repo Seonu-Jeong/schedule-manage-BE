80d452d655f854d9814c0d8b83fd88758e1199bb
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-11 14:10:28 +0900

    Feat : Define findAll func to ScheduleService

--- ScheduleManage/src/main/java/com/example/schedule/service/ScheduleService.java
+++ ScheduleManage/src/main/java/com/example/schedule/service/ScheduleService.java
@@ -1,7 +1,12 @@
 package com.example.schedule.service;
 
 import com.example.schedule.dto.ScheduleRequestDto;
+import com.example.schedule.dto.ScheduleResponseDto;
+
+import java.util.List;
 
 public interface ScheduleService {
     long saveSchedule(ScheduleRequestDto scheduleRequestDto);
+
+    List<ScheduleResponseDto> findAll(String author, String modificationDate);
 }
