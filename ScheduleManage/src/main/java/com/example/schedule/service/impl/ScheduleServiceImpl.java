3cfcf486720495d7a55685d8faeb180aae4c38e9
Jeong Seon U <sw.jeong97@gmail.com>
2024-11-11 13:20:47 +0900

    Feat : Implement saveSchedule func to ScheduleServiceImpl

---
+++ ScheduleManage/src/main/java/com/example/schedule/service/impl/ScheduleServiceImpl.java
@@ -0,0 +1,34 @@
+package com.example.schedule.service.impl;
+
+import com.example.schedule.dto.ScheduleRequestDto;
+import com.example.schedule.entity.User;
+import com.example.schedule.repository.ScheduleRepository;
+import com.example.schedule.repository.UserRepository;
+import com.example.schedule.service.ScheduleService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    private final UserRepository userRepository;
+
+    public ScheduleServiceImpl(ScheduleRepository scheduleRepository, UserRepository userRepository){
+        this.scheduleRepository = scheduleRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public long saveSchedule(ScheduleRequestDto scheduleRequestDto) throws NoSuchElementException {
+        Optional<User> user = userRepository.findByAuthorAndPassword(
+                scheduleRequestDto.getAuthor(), scheduleRequestDto.getPassword());
+
+        user.orElseThrow(() -> new NoSuchElementException("존재하지 않는 유저입니다"));
+
+        return scheduleRepository.saveSchedule(user.get().getId(), scheduleRequestDto.getTodo());
+    }
+}
